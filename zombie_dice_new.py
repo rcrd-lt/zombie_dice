d287939be325bf9bc79bc5eb49ad3f11c56f4cab
rcrd-lt <ricardoleite92@live.com>
2021-09-21 22:12:20 -0300

    funcao lanca_dados adicionada

--- zombie_dice_new.py
+++ zombie_dice_new.py
@@ -1,20 +1,29 @@
 # Disciplina de Raciocíno computacional - PUCPR - Aluno: Ricardo A. Soares Leite
 import random
-from collections import namedtuple
+
 comeca_partida = False
 
 #define os valores para os dados
-dado_vermelho = namedtuple("Vermelho", ["Cerebro", "Passos", "Cerebro", "Tiro", "Passos", "Cerebro"])
-dado_verde = namedtuple("Verde", ["Tiro", "Passos", "Cerebro", "Passos", "Cerebro", "Tiro"])
-dado_amarelo = namedtuple("Amarelo", ["Tiro", "Passos", "Tiro", "Cerebro", "Passos", "Tiro"])
+dado_vermelho = ("Cerebro", "Passos", "Cerebro", "Tiro", "Passos", "Cerebro")
+dado_verde = ("Tiro", "Passos", "Cerebro", "Passos", "Cerebro", "Tiro")
+dado_amarelo = ("Tiro", "Passos", "Tiro", "Cerebro", "Passos", "Tiro")
 
 #define as listas
 copo_dados = []
 dados_jogados = []
 lista_jogadores = []
 
+cerebros = 0
+tiros = 0
+
 comeca_partida = False
 turno = False
+
+
+# mostrar os dados;
+# mostrar os dados no copo;
+# pegar cada um dos tipos de dados (verde, amarelo, vermelho);
+# verificar score (cérebros, tiros e passos).
 
 def adicionar_dados():    #adicionar os dados
     for i in range(0, 6):
@@ -25,8 +34,23 @@ def adicionar_dados():    #adicionar os dados
         copo_dados.append(dado_vermelho)
 
 
+def lanca_dados():
+    num_sorteado = int(random.randrange(len(copo_dados)))
+    print("Dado sorteado {}: {} ".format((i + 1), num_sorteado))
 
+    dado_sorteado = copo_dados[num_sorteado]  # 'lança' o dado e verifica qual a face sorteada
+    face_dado = int(random.randrange(0, 5))  # obtem a face do dado
 
+    if dado_sorteado[face_dado] == "C":  # tirou "cerebro no dado
+        print("Você comeu um cérebro!!")
+        copo_dados.remove(dado_sorteado)
+
+    elif dado_sorteado[face_dado] == "T":  # tirou tiro no dado
+        print("Você levou um tiro!!")
+        copo_dados.remove(dado_sorteado)
+
+    else:
+        print("A vítima escapou!!")
 
 
 
@@ -38,13 +62,13 @@ while len(lista_jogadores) == 0:
         numero_jogadores = int(input("Digite o número de jogadores na partida: \n"))
         if numero_jogadores >= 2:
             for i in range(0, numero_jogadores):
-                nomes = input("Qual o nome do jogador?" )
-                lista_jogadores.append(nomes)
+                nome = input("Qual o nome do jogador?" )
+                lista_jogadores.append(nome)
         else:
             print("O jogo precisa de 2 ou mais jogadores!\n")
-            comeca_partida = True
-            turno = True
-            adicionar_dados()
+        comeca_partida = True
+        turno = True
+        adicionar_dados()
     except:
         print("O jogo precisa de 2 ou mais jogadores!\n")
 
@@ -64,61 +88,14 @@ while comeca_partida == True:   #PAREI AQUI RS
             print("valor inválido!")
         if jogar == 1:
             for i in range(0, 3):
-                num_sorteado = int(random.randrange(len(copo_dados)))
-                print("Dado sorteado {}: {} ".format((i + 1), num_sorteado))
-
-                dado_sorteado = copo_dados[num_sorteado]  # 'lança' o dado e verifica qual a face sorteada
-                face_dado = int(random.randrange(0, 5))  # obtem a face do dado
-
-                if dado_sorteado[face_dado] == "C":  # tirou "cerebro no dado
-                    print("Você comeu um cérebro!!")
-                    cerebros = cerebros + 1
-                    copo_dados.remove(dado_sorteado)
-
-                elif dado_sorteado[face_dado] == "T":  # tirou tiro no dado
-                    print("Você levou um tiro!!")
-                    tiros = tiros + 1
-                    copo_dados.remove(dado_sorteado)
-
-                else:
-                    print("A vítima escapou!!")
-
+                lanca_dados()
 
         elif jogar == 2:
             pontos = cerebros
             print("Turno finalizado! Fez", pontos, "pontos!")
             print(lista_jogadores)
             turno += 1
-            tiros = 0
 
         elif jogar == 3:
             print("Próximo jogador!")
             adicionar_dados()
-            vez = lista_jogadores.index(player[i])
-
-
-
-
-
-
-
-
-        #try:
-         #   if():
-          #  else:
-
-        #except:
-        #    print()
-
-        # if numero_jogadores >= 2:
-        #     comeca_partida = True # iniciar a partida
-        #     cerebros = 0
-        #     tiros = 0
-        #     adicionar_dados()
-        #     i = 1
-        #     while (i < numero_jogadores +1):
-        #         player = dict({'jogador': i, 'cerebros': 0, 'tiros': 0})
-        #         lista_jogadores.append(player)
-        #         i = i + 1
-
-
