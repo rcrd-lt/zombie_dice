2c3a97a4232662e02c8fa7207d5fe11e5d87507a
rcrd-lt <ricardoleite92@live.com>
2021-09-24 22:00:46 -0300

    Reescrevendo o jogo utilizando classes e métodos

---
+++ z_dice_v2.py
@@ -0,0 +1,27 @@
+import random
+
+#definicao de classes e métodos
+class Jogador:
+    def __init__(self, nome, cerebro, tiro, passo):
+        self.nome = nome
+        self.cerebro = cerebro
+        self.tiro = tiro
+        self.passo = passo
+
+class Dado:
+    def __init__(self):
+        self.dado_gr = ("T", "P", "C", "P", "C", "T")
+        self.dado_yw = ("T", "P", "T", "C", "P", "T")
+        self.dado_rd = ("C", "P", "C", "T", "P", "C")
+
+#definicao de listas
+copo_dados = []
+lista_jogadores = []
+
+#definicao de variaveis globais
+
+
+
+#lógica do jogo
+
+
--- zombie_dice_new.py
+++ zombie_dice_new.py
@@ -1,31 +1,32 @@
 # Disciplina de Raciocíno computacional - PUCPR - Aluno: Ricardo A. Soares Leite
 import random
 
-comeca_partida = False
-
-#define os valores para os dados
+# define os valores para os dados
 dado_vermelho = ("Cerebro", "Passos", "Cerebro", "Tiro", "Passos", "Cerebro")
 dado_verde = ("Tiro", "Passos", "Cerebro", "Passos", "Cerebro", "Tiro")
 dado_amarelo = ("Tiro", "Passos", "Tiro", "Cerebro", "Passos", "Tiro")
 
-#define as listas
+# define as listas
 copo_dados = []
 dados_jogados = []
 lista_jogadores = []
-
-cerebros = 0
-tiros = 0
+cerebros = []
+tiros = []
+passos = []
 
 comeca_partida = False
 turno = False
 
-
-# mostrar os dados;
-# mostrar os dados no copo;
 # pegar cada um dos tipos de dados (verde, amarelo, vermelho);
 # verificar score (cérebros, tiros e passos).
 
-def adicionar_dados():    #adicionar os dados
+def adicionar_jogador():
+    for jogador in range(0, numero_jogadores):
+        nome = input("Qual o nome do jogador? \n")
+        lista_jogadores.append(nome)
+
+
+def adicionar_dados():  # adicionar os dados
     for i in range(0, 6):
         copo_dados.append(dado_verde)
     for i in range(0, 4):
@@ -35,35 +36,37 @@ def adicionar_dados():    #adicionar os dados
 
 
 def lanca_dados():
+    c = 0
+    t = 0
+    p = 0
     num_sorteado = int(random.randrange(len(copo_dados)))
-    print("Dado sorteado {}: {} ".format((i + 1), num_sorteado))
-
     dado_sorteado = copo_dados[num_sorteado]  # 'lança' o dado e verifica qual a face sorteada
     face_dado = int(random.randrange(0, 5))  # obtem a face do dado
 
-    if dado_sorteado[face_dado] == "C":  # tirou "cerebro no dado
+    if dado_sorteado[face_dado] == "Cerebro":  # tirou "cerebro no dado
         print("Você comeu um cérebro!!")
         copo_dados.remove(dado_sorteado)
+        cerebros.append(c)
 
-    elif dado_sorteado[face_dado] == "T":  # tirou tiro no dado
+    elif dado_sorteado[face_dado] == "Tiro":  # tirou tiro no dado
         print("Você levou um tiro!!")
         copo_dados.remove(dado_sorteado)
+        tiros.append(t)
 
     else:
         print("A vítima escapou!!")
+        passos.append(p)
 
+
+def mostra_copo():              #adicionar ao fim da rodada
+    print("Os dados restantes são: {} \n".format(copo_dados))
 
 
 while len(lista_jogadores) == 0:
-    # numero_jogadores = int(input("Digite o número de jogadores na partida: \n"))
-    # for ind in range(0, numero_jogadores):
-    #     nome = input("Qual o nome do jogador? \n")
     try:
         numero_jogadores = int(input("Digite o número de jogadores na partida: \n"))
         if numero_jogadores >= 2:
-            for i in range(0, numero_jogadores):
-                nome = input("Qual o nome do jogador?" )
-                lista_jogadores.append(nome)
+            adicionar_jogador()
         else:
             print("O jogo precisa de 2 ou mais jogadores!\n")
         comeca_partida = True
@@ -72,30 +75,25 @@ while len(lista_jogadores) == 0:
     except:
         print("O jogo precisa de 2 ou mais jogadores!\n")
 
-
 print("Partida iniciada! ")
-while comeca_partida == True:   #PAREI AQUI RS
+while comeca_partida == True:
+    try:
+        adicionar_dados()
+        print("\n olá, digite o que deseja fazer:")
+        jogar = int(input("[1] - Jogar os dados, [2] Finalizar turno \n"))
+    except:
+        print("valor inválido!")
 
-    #Rever loops
-    #adicionar turnos
-    #troca de jogadores
-
-    for i in range(len(lista_jogadores)):
-        try:
-            print("\n olá, {}, digite o que deseja fazer:".format(nome))
-            jogar = int(input("[1] - Jogar os dados, [2] Finalziar turno ou [3] Trocar de jogador: \n"))
-        except:
-            print("valor inválido!")
         if jogar == 1:
             for i in range(0, 3):
                 lanca_dados()
+                mostra_copo()
 
         elif jogar == 2:
-            pontos = cerebros
-            print("Turno finalizado! Fez", pontos, "pontos!")
-            print(lista_jogadores)
-            turno += 1
-
-        elif jogar == 3:
-            print("Próximo jogador!")
-            adicionar_dados()
+            print("O jogo foi finalizado")
+            comeca_partida = False
+            restart = input("Deseja iniciar a partida novamente? [s] Sim - [n] Não")
+            if restart == 's':
+                comeca_partida == True
+            elif restart == 'n':
+                print("Obirgado por jogar! Até a próxima!")
